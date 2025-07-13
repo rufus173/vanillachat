4c542302b243b09c0c4066236d9b5e5404cb0401
rufus173 <derrickotron5000@gmail.com>
2025-07-13 14:24:48 +0100

    restructure + basic connection handling with the daemon set up

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 # What is it?
 
 Just like vanilla, it's a basic flavour of a tcp chat client. Maybe it will get upnp support? Who knows. For now, it is only useful on the same network. Due to use of `termios`, this will not function on windows.
+It has a daemon and a client, with the daemon accepting connections, and notifying the user of them. The client program can then be passed the connection from the daemon and chat. Imagine it as a phone that rings when you have a call, and puts the caller on hold untill you pick up the phone.
 
 # Install
 
---
+++ vanillachatd/src/main.rs
@@ -0,0 +1,67 @@
+use std::io;
+use std::time::Duration;
+use std::thread;
+use std::sync::Mutex;
+use std::cell::RefCell;
+use std::net::{TcpListener, TcpStream, SocketAddr};
+
+pub struct Connection {
+	stream: TcpStream,
+	address: SocketAddr,
+}
+
+fn main() -> io::Result<()>{
+	let mut connections: Vec<Connection> = vec![];
+	//===== setup the listener ======
+	let port: u16 = 9567;
+	let addr = SocketAddr::from(([0,0,0,0],port));
+	let listener = TcpListener::bind(addr)?;
+	//nonblocking
+	listener.set_nonblocking(true).expect("could not set listener to nonblocking");
+	loop{
+		//====== accept tcp connections ======
+		let _ = match listener.accept(){
+			Ok(connection) => handle_connection(&mut connections,connection.0,connection.1),
+			Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => Ok(()),
+			Err(e) => panic!("Error: {e}"),
+		};
+		//====== accept ipc connections ======
+		//====== verify sockets are still alive ======
+		let mut connections_to_delete = vec![];
+		for connection in connections.iter().enumerate(){
+			if !is_alive(connection.1){
+				println!("connection {} dead",connection.1.address);
+				connections_to_delete.push(connection.0);
+			}
+		}
+		for connection_to_delete in connections_to_delete{
+			connections.swap_remove(connection_to_delete);
+		}
+
+		//====== yield cpu time to other processes ======
+		thread::sleep(Duration::from_millis(20));
+	}
+}
+fn handle_connection(connections: &mut Vec<Connection>, stream: TcpStream, address: SocketAddr) -> Result<(), io::Error>{
+	println!("New connection: {}",address);
+	let connection = Connection {
+		stream: stream,
+		address: address,
+	};
+	connections.push(connection);
+	Ok(())
+}
+fn is_alive(connection: &Connection) -> bool{
+	connection.stream.set_nonblocking(true).expect("could not place connection socket into nonblocking mode");
+	let mut buf = [0; 1];
+	let result = connection.stream.peek(&mut buf);
+	connection.stream.set_nonblocking(false).expect("could not place connection socket into blocking mode");
+	match result {
+		Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => true,
+		Ok(size) => match size {
+			0 => false,
+			_ => true,
+		},
+		Err(e) => false,
+	}
+}
